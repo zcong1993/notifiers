d536da0b21ee2cb4a830048cdbb3d20122f8ae37
zcong1993 <zhangcong1992@gmail.com>
2021-03-06 14:49:15 +0800

    chore(tg): html to text before send

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/k3a/html2text v1.0.7 // indirect
 	github.com/mailgun/mailgun-go/v4 v4.3.3
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.7.0
--- telegram.go
+++ telegram.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"strconv"
 
+	"github.com/k3a/html2text"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/pkg/errors"
 )
@@ -54,8 +56,8 @@ func (tg *Telegram) Notify(ctx context.Context, to string, msg Message) error {
 		toId = to
 	}
 
-	tgMsg := tgbotapi.NewMessage(toId, msg.Content)
-	tgMsg.ParseMode = tgbotapi.ModeHTML
+	tgMsg := tgbotapi.NewMessage(toId, html2text.HTML2Text(msg.Content))
+	//tgMsg.ParseMode = tgbotapi.ModeHTML
 
 	_, err := tg.tgClient.Send(tgMsg)
 	if err != nil {
